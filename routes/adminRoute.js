7f1bb8d12d7c8d278a940ca2849a3e14dd32deda
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2020-10-31 23:35:14 +0800

    removed admin credentials

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,19 +3,6 @@ const router = express.Router()
 const passport = require('passport')
 const Admin = require('../models/adminSchema')
 
-// ADMIN LOGIN
-// hardcoded admin registration
-console.log('Registering hardcoded admin account')
-Admin.register(new Admin({username: 'admin'}), 'gagoka1434', (err, user) => {
-    if(err){
-        console.log(err)
-    }
-
-    passport.authenticate('local')(req,res, ()=> {
-        res.redirect('/api/blog')
-    })
-})
-
 //LOGIN ROUTE
 router.get('/api/login', (req,res)=> {
     res.render('adminLogin')
